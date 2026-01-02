d9ddbb662c616ce5a72828def76bc53feec0e316
Herédi Áron <aronheredi346@gmail.com>
2026-01-02 20:22:28 +0100

    fix:  query builder now outputs the correct json format for the backend

--- frontend/template-frontend/src/components/ui/canvas/query/QueryBuilderPopup.tsx
+++ frontend/template-frontend/src/components/ui/canvas/query/QueryBuilderPopup.tsx
@@ -7,10 +7,12 @@ import type { QueryProperty } from '@/types/query';
 import { Plus, ChevronDown } from 'lucide-react';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { fabricQuerySchema } from '@/schemas/query-schema';
+import { useParams } from 'react-router-dom';
 interface QueryBuilderPopupProps {
     canvas: Canvas
 }
 export const QueryBuilderPopup = ({ canvas }: QueryBuilderPopupProps) => {
+    const { id } = useParams();
     const [open, setOpen] = useState(false);
     const [json, setJson] = useState('');
     const onSubmit = (data) => {
@@ -20,10 +22,21 @@ export const QueryBuilderPopup = ({ canvas }: QueryBuilderPopupProps) => {
     const [values, setValues] = useState<Record<string, QueryProperty[]>>({});
     useEffect(() => {
         //leave out unneccessary fields for the backend
-        setJson(JSON.stringify(values, (key, value) => {
-            if (key === 'label' || key === 'type') return undefined;
-            return value;
-        }, 2));
+        const transformedValues = Object.entries(values).map(([objName, props]) => {
+            const propertyObject: Record<string, string | number | boolean | undefined> = { name: objName };
+            props.forEach(prop => {
+                propertyObject[prop.key] = prop.value;
+            });
+            return propertyObject;
+
+        });
+        const jsonObject = {
+            templateId: id,
+            values: transformedValues
+        }
+
+
+        setJson(JSON.stringify(jsonObject, null, 2));
     }, [values]);
     useEffect(() => {
         if (!open) return;
@@ -71,11 +84,11 @@ export const QueryBuilderPopup = ({ canvas }: QueryBuilderPopupProps) => {
 
                         <h3 className="font-semibold text-sm text-slate-900 mb-4">Elements</h3>
                         <div className='flex flex-col gap-4 overflow-scroll max-h-[40vh]'>
-                            {canvas.getObjects().map((obj, idx) => (
-                                <div key={idx} className='border border-slate-200 bg-slate-50 rounded-lg p-4 w-full flex flex-col gap-2'>
+                            {canvas.getObjects().map((obj) => (
+                                <div key={obj.name} className='border border-slate-200 bg-slate-50 rounded-lg p-4 w-full flex flex-col gap-2'>
                                     <label className="flex flex-row gap-2 text-sm font-medium text-slate-700 mb-1 " htmlFor="name">{obj.name} <p className='text-slate-300'>({obj.type})</p></label>
-                                    {values[obj.name!]?.map((prop, pIdx) => (
-                                        <TextField.Root onChange={(e) => setPropertyValue(obj.name!, prop.key, e.target.value)} key={pIdx} placeholder='Prop name' radius='large' size='3' className=' gap-2 border-slate-300 py-4 px-3 shadow-sm focus:border-primary focus:ring-1 focus:ring-primary'>
+                                    {values[obj.name!]?.map((prop) => (
+                                        <TextField.Root onChange={(e) => setPropertyValue(obj.name!, prop.key, e.target.value)} key={prop.key} placeholder='Prop name' radius='large' size='3' className=' gap-2 border-slate-300 py-4 px-3 shadow-sm focus:border-primary focus:ring-1 focus:ring-primary'>
                                             <TextField.Slot className='font-mono text-slate-600 min-w-[80px] p-1 flex-1 overflow-clip ' >
                                                 {prop.key}
                                             </TextField.Slot>
@@ -97,11 +110,11 @@ export const QueryBuilderPopup = ({ canvas }: QueryBuilderPopupProps) => {
                                                 align="start"
                                                 className=" w-full bg-white rounded-md shadow-md border border-slate-200 p-2 w-[var(--radix-popover-trigger-width)]"
                                             >
-                                                {fabricQuerySchema[obj.type].filter((prop) => !(values[obj.name!]?.some((p) => p.key === prop.key))).map((prop, propIdx) => {
+                                                {fabricQuerySchema[obj.type].filter((prop) => !(values[obj.name!]?.some((p) => p.key === prop.key))).map((prop) => {
 
                                                     return (
 
-                                                        <DropdownMenu.Item key={propIdx} onSelect={(e) => addProperty(obj.name!, { ...prop, value: obj.get(prop.key) })} className="px-3 py-2 rounded-md hover:bg-slate-100 text-sm cursor-pointer">
+                                                        <DropdownMenu.Item key={prop.key} onSelect={() => addProperty(obj.name!, { ...prop, value: obj.get(prop.key) })} className="px-3 py-2 rounded-md hover:bg-slate-100 text-sm cursor-pointer">
                                                             {prop.key}
                                                         </DropdownMenu.Item>
                                                     );
@@ -114,20 +127,7 @@ export const QueryBuilderPopup = ({ canvas }: QueryBuilderPopupProps) => {
                             ))}
                         </div>
 
-                        {/* {<div className="flex justify-end gap-2">
-                            <Dialog.Close asChild>
-                                <button type="button" className="rounded-md border px-3 py-2 text-sm">
-                                    Cancel
-                                </button>
-                            </Dialog.Close>
 
-                            <button
-                                type="submit"
-                                className="inline-flex items-center justify-center rounded-md bg-slate-900 px-3 py-2 text-sm text-white"
-                            >
-                                Create Template
-                            </button>
-                        </div>} */}
                     </form>
                     <div className='flex flex-col space-y-4 p-4'>
                         <h3 className="font-semibold text-sm text-slate-900 mb-4">JSON Query</h3>
