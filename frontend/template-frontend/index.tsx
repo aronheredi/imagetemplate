8eeb9bcc4e415fb53a4f3b5777809c91734141b9
Herédi Áron <aronheredi346@gmail.com>
2026-01-02 20:03:41 +0100

    fix: added offline access token acces so the site is able to request a new one

--- frontend/template-frontend/index.tsx
+++ frontend/template-frontend/index.tsx
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
       authorizationParams={{
         redirect_uri: window.location.origin,
         audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        scope: 'openid profile email offline_access',
       }}
       cacheLocation='localstorage'
       useRefreshTokens={true}
